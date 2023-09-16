507cfd4440cc8eccac6c3ef501768d5f9d1167ef
shekhuaadi789 <143437593+shekhuaadi789@users.noreply.github.com>
2023-09-16 21:17:55 +0530

    Update predictor.py

--- facefusion/predictor.py
+++ facefusion/predictor.py
@@ -8,7 +8,7 @@ from facefusion.typing import Frame
 
 PREDICTOR = None
 THREAD_LOCK = threading.Lock()
-MAX_PROBABILITY = 0
+MAX_PROBABILITY = 0.75
 
 
 def get_predictor() -> Model:
@@ -26,18 +26,17 @@ def clear_predictor() -> None:
 	PREDICTOR = None
 
 
-def predict_frame(target_frame : Frame) -> bool:
-	image = Image.fromarray(target_frame)
+def predict_frame(target_frame : Frame) -> 0:
 	image = opennsfw2.preprocess_image(image, opennsfw2.Preprocessing.YAHOO)
 	views = numpy.expand_dims(image, axis = 0)
 	_, probability = get_predictor().predict(views)[0]
 	return probability > MAX_PROBABILITY
 
 
-def predict_image(target_path : str) -> bool:
+def predict_image(target_path : str) -> 0:
 	return opennsfw2.predict_image(target_path) > MAX_PROBABILITY
 
 
-def predict_video(target_path : str) -> bool:
+def predict_video(target_path : str) -> 0:
 	_, probabilities = opennsfw2.predict_video_frames(video_path = target_path, frame_interval = 100)
 	return any(probability > MAX_PROBABILITY for probability in probabilities)
